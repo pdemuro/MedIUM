12bf7bed93ebeff32530bf497023dcabc8f2f528
pdemuro <pdemuro99@gmail.com>
2020-02-08 18:48:31 +0100

    implementazione registro prenotazioni

--- app/src/main/java/com/medium/progettomedium/Fragment/FavouriteFragment.java
+++ app/src/main/java/com/medium/progettomedium/Fragment/FavouriteFragment.java
@@ -1,21 +1,195 @@
 package com.medium.progettomedium.Fragment;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.medium.progettomedium.ActivityDettagliEvento;
+import com.medium.progettomedium.Adapter.AdaptEvento;
+import com.medium.progettomedium.Model.DatabaseEvento;
+import com.medium.progettomedium.Model.DatabaseUtente;
 import com.medium.progettomedium.R;
 
+import java.util.ArrayList;
+
 public class FavouriteFragment extends Fragment {
+
+    boolean doubleTap = false;
+    private ArrayList<DatabaseEvento> eventi = new ArrayList<DatabaseEvento>();
+    private AdaptEvento adapter;
+    private AdaptEvento.OnItemClickListener itemClickListener;
+    private RecyclerView listaEventiView;
+    DatabaseReference mRef;
+    private DatabaseReference databaseReference;
+    private Button statoprenotazione;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favourite, container, false);
+        View view =inflater.inflate(R.layout.fragment_home, container, false);
+        listaEventiView = (RecyclerView) view.findViewById(R.id.row_adda);
+        listaEventiView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        listaEventiView.setLayoutManager(linearLayoutManager);
+        eventi = new ArrayList<DatabaseEvento>();
+
+        FirebaseMessaging.getInstance().subscribeToTopic("MyTopic");
+
+        DatabaseEvento.date_collection_arr = new ArrayList<DatabaseEvento>();
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference("Eventi");
+        //POPOLAZIONE EVENTI DA DATABASE
+        databaseReference.addChildEventListener(new ChildEventListener() {
+
+
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                loadData(dataSnapshot);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                loadData(dataSnapshot);
+            }
+
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                removeData(dataSnapshot);
+            }
+
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            public void onCancelled(DatabaseError databaseError) {
+            }
+
+
+        });
+
+        return view;
+
     }
 
+
+    public void loadData(DataSnapshot dataSnapshot) {
+        // get all of the children at this level.
+
+        final DatabaseEvento doc = dataSnapshot.getValue(DatabaseEvento.class);
+        final DatabaseEvento eve = dataSnapshot.getValue(DatabaseEvento.class);
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        final FirebaseUser user = firebaseAuth.getCurrentUser();
+        final String nome1= user.getDisplayName().replaceAll("%20" ," ");
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+
+             databaseReference.child("UserID").child("Utenti").child(nome1).child("prenotazioni").addValueEventListener(new ValueEventListener() {
+
+            /**
+             * This method will be invoked any time the data on the database changes.
+             * Additionally, it will be invoked as soon as we connect the listener, so that we can get an initial snapshot of the data on the database.
+             * @param dataSnapshot
+             */
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // get all of the children at this level.
+                Iterable<DataSnapshot> children = dataSnapshot.getChildren();
+                // shake hands with each of them.'
+                int var = 0;
+                for (DataSnapshot child : children) {
+
+                        if (doc.getId().equals(child.getKey())){
+                            var = 1;
+                        }
+
+                }
+                if(var == 1) {
+                    DatabaseEvento.date_collection_arr.add(eve);
+                    eventi.add(doc);
+                }
+                adapter = new AdaptEvento(getContext(), eventi, itemClickListener);
+
+                //listaEventiView.setAdapter(adapter);
+                listaEventiView.setAdapter(new AdaptEvento(getContext(), eventi, new AdaptEvento.OnItemClickListener() {
+                    @Override public void onItemClick(DatabaseEvento item) {
+
+                        String mTitolo = item.getTitolo();
+                        String mLuogo = item.getLuogo();
+                        String mData = item.getDate();
+                        String mImage = item.getImmagine();
+                        Intent intent = new Intent(listaEventiView.getContext(), ActivityDettagliEvento.class);
+                        intent.putExtra("titolo", mTitolo);
+                        intent.putExtra("luogo", mLuogo);
+                        intent.putExtra("data", mData);
+                        intent.putExtra("immagine", mImage);
+                        startActivity(intent);
+                    }
+
+
+                }));
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+
+
+            });
+
+
+        adapter = new AdaptEvento(getContext(), eventi, itemClickListener);
+
+        //listaEventiView.setAdapter(adapter);
+        listaEventiView.setAdapter(new AdaptEvento(getContext(), eventi, new AdaptEvento.OnItemClickListener() {
+            @Override public void onItemClick(DatabaseEvento item) {
+
+                String mTitolo = item.getTitolo();
+                String mLuogo = item.getLuogo();
+                String mData = item.getDate();
+                String mImage = item.getImmagine();
+                Intent intent = new Intent(listaEventiView.getContext(), ActivityDettagliEvento.class);
+                intent.putExtra("titolo", mTitolo);
+                intent.putExtra("luogo", mLuogo);
+                intent.putExtra("data", mData);
+                intent.putExtra("immagine", mImage);
+                startActivity(intent);
+            }
+
+
+        }));
+    }
+    public void removeData(DataSnapshot dataSnapshot) {
+        // get all of the children at this level.
+
+        DatabaseEvento doc = dataSnapshot.getValue(DatabaseEvento.class);
+        DatabaseEvento eve = dataSnapshot.getValue(DatabaseEvento.class);
+        DatabaseEvento.date_collection_arr.remove(eve);
+        eventi.remove(doc);
+
+        adapter = new AdaptEvento(getContext(), eventi, itemClickListener);
+        listaEventiView.setAdapter(adapter);
+    }
 }
