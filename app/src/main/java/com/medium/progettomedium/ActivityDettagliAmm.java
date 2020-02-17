368e6ee5c1ad985457493fa68d131530e3974ed2
mirkoPili <mirkpili96@gmail.com>
2020-02-17 21:29:23 +0100

    PROBLEMA

--- app/src/main/java/com/medium/progettomedium/ActivityDettagliAmm.java
+++ app/src/main/java/com/medium/progettomedium/ActivityDettagliAmm.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -24,12 +25,13 @@ import java.util.ArrayList;
 
 public class ActivityDettagliAmm extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
-    private AdaptAmm ammAdapter;
-    private ArrayList<DatabaseUtente> utenti = new ArrayList<DatabaseUtente>();
-    private DatabaseReference databaseReferenceutente;
-    private DatabaseReference databaseReference;
-    private AdaptAmm.OnItemClickListener itemClickListener;
+     RecyclerView recyclerView,elencoAccettati;
+     Button accettati,prenotati;
+     AdaptAmm ammAdapter;
+     ArrayList<DatabaseUtente> utenti = new ArrayList<DatabaseUtente>();
+     DatabaseReference databaseReferenceutente;
+     DatabaseReference databaseReference;
+     AdaptAmm.OnItemClickListener itemClickListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -37,11 +39,20 @@ public class ActivityDettagliAmm extends AppCompatActivity {
         setContentView(R.layout.activity_dettagli_amm);
 
         recyclerView = findViewById(R.id.elenco_richieste);
+
+
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
 
-        recyclerView.setAdapter(ammAdapter);
+        elencoAccettati.findViewById(R.id.elencoAccettati);
+        elencoAccettati.setHasFixedSize(true);
+        elencoAccettati.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
 
+        recyclerView.setAdapter(ammAdapter);
+        elencoAccettati.setAdapter(ammAdapter);
+
+        accettati.findViewById(R.id.accettati);
+        prenotati.findViewById(R.id.prenotati);
 
         recyclerView.setHasFixedSize(true);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
@@ -50,11 +61,109 @@ public class ActivityDettagliAmm extends AppCompatActivity {
         recyclerView.setLayoutManager(linearLayoutManager);
         utenti = new ArrayList<DatabaseUtente>();
 
+        elencoAccettati.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager1 = new LinearLayoutManager(getApplicationContext());
+        linearLayoutManager1.setReverseLayout(true);
+        linearLayoutManager1.setStackFromEnd(true);
+        elencoAccettati.setLayoutManager(linearLayoutManager1);
+        utenti = new ArrayList<DatabaseUtente>();
+
+
+        /*prenotati.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                elencoAccettati.setVisibility(View.GONE);
+                recyclerView.setVisibility(View.VISIBLE);
+                elencoRic();
+            }
+        });
+        accettati.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                elencoAccettati.setVisibility(View.VISIBLE);
+                recyclerView.setVisibility(View.GONE);
+                elencoAccetta();
+
+            }
+        });*/
         String title = getIntent().getStringExtra("title");
         String place = getIntent().getStringExtra("description");
         String description = getIntent().getStringExtra("descrizione");
         String image = getIntent().getStringExtra("image");
         String data1 = getIntent().getStringExtra("date");
+        final String id = getIntent().getStringExtra("id");
+
+
+    }
+    public void elencoAccetta(){
+        final String id = getIntent().getStringExtra("id");
+        databaseReferenceutente = FirebaseDatabase.getInstance().getReference();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        databaseReferenceutente.child("UserID").child("Utenti").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Iterable<DataSnapshot> children = dataSnapshot.getChildren();
+                // shake hands with each of them.'
+                int var = 0;
+                for (DataSnapshot child : children) {
+                    final DatabaseUtente utente = child.getValue(DatabaseUtente.class);
+                    if(utente.getCategory().equals("Utente")) {
+                        databaseReference.child("UserID").child("Utenti").child(utente.nome+" "+utente.cognome).child("prenotazioni").addValueEventListener(new ValueEventListener() {
+
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                // get all of the children at this level.
+                                Iterable<DataSnapshot> children = dataSnapshot.getChildren();
+                                // shake hands with each of them.'
+
+                                for (DataSnapshot child : children) {
+                                    if(id.equals(child.getKey())) {
+                                        String stato = child.getValue().toString();
+                                        if(stato.equals("3")){
+                                            utenti.add(utente);
+                                        }
+                                    }
+
+                                }
+
+
+                                ammAdapter = new AdaptAmm(getApplication(), utenti,id, itemClickListener);
+
+                                //listaEventiView.setAdapter(adapter);
+                                recyclerView.setAdapter(new AdaptAmm(getApplication(), utenti,id, new AdaptAmm.OnItemClickListener() {
+                                    @Override public void onItemClick(DatabaseUtente item) {
+
+                                        String mNome = item.getNome()+" "+item.getCognome();
+                                        String mMail = item.getMail();
+                                        String mPhone = item.getPhone();
+                                        Intent intent = new Intent(recyclerView.getContext(), ActivityDettagliAmm.class);
+                                        intent.putExtra("nome", mNome);
+                                        intent.putExtra("mail", mMail);
+                                        intent.putExtra("phone", mPhone);
+                                        startActivity(intent);
+                                    }
+
+
+                                }));
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+
+                            }
+
+
+                        });
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+    public void elencoRic(){
         final String id = getIntent().getStringExtra("id");
         databaseReferenceutente = FirebaseDatabase.getInstance().getReference();
         databaseReference = FirebaseDatabase.getInstance().getReference();
@@ -121,8 +230,5 @@ public class ActivityDettagliAmm extends AppCompatActivity {
 
             }
         });
-
-
-
     }
 }
